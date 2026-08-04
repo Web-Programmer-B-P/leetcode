92fa86e90c60d357b0e701c7c20625d97e219dea
pbarsukov <barsukov2805@gmail.com>
2026-08-04 20:40:18 +0300

    GroupAnagrams

---
+++ src/main/java/com/example/group_anagrams/GroupAnagrams.java
@@ -0,0 +1,59 @@
+package com.example.group_anagrams;
+
+import java.util.*;
+
+
+/**
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ * strs[i] consists of lowercase English letters.
+ */
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        System.out.println(groupAnagramsBySorting(strs));
+        System.out.println(groupAnagramsByFrequency(strs));
+    }
+
+    public static List<List<String>> groupAnagramsBySorting(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String item : strs) {
+            char[] chars = item.toCharArray();
+            Arrays.sort(chars);
+            // Добавляем строку в соответствующую группу
+            map.computeIfAbsent(new String(chars), k -> new ArrayList<>()).add(item);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static List<List<String>> groupAnagramsByFrequency(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i : count) {
+                sb.append(i).append('#');
+            }
+
+            String key = sb.toString();
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+
+}
