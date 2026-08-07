dc44cc9234c1a36327c8d25a05274ae50188a60c
Petr Barsukov <barsukov2805@gmail.com>
2026-08-07 14:59:26 +0300

    MergeIntervals

---
+++ src/main/java/com/example/merge_intervals/MergeIntervals.java
@@ -0,0 +1,39 @@
+package com.example.merge_intervals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+ * and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ */
+public class MergeIntervals {
+    public static void main(String[] args) {
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+
+        System.out.println(Arrays.deepToString(merge(intervals)));
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> result = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        result.add(currentInterval);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] nextInterval = intervals[i];
+            if (nextInterval[0] <= currentInterval[1]) {
+                currentInterval[1] = Math.max(currentInterval[1], nextInterval[1]);
+            } else {
+                currentInterval = nextInterval;
+                result.add(currentInterval);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+}
